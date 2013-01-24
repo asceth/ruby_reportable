62325ffdf19da25d25ac7874db4a1d3419537bba
David McWilliams <dmcwilliams@gmail.com>
2013-01-24 15:47:58 -0500

    add more detail to the example report

--- examples/object_space_report.rb
+++ examples/object_space_report.rb
@@ -1,8 +1,19 @@
 class ObjectSpaceReport
   include RubyReportable
 
-  name 'Object Space By Class'
+  #
+  # Name the report
+  #
+  report 'Object Space By Class'
 
+  #
+  # Give the report a category for organization
+  #
+  category 'Object Reports'
+
+  #
+  # Define the data source
+  #
   source do
     as :object
 
@@ -11,6 +22,38 @@ class ObjectSpaceReport
     end
   end
 
+  #
+  # Perform a final manipulation to the returned records like making sure all are unique
+  #
+  finalize
+    source.all.uniq
+  end
+
+  #
+  # Define output fields
+  #
   output('Class') { object.first.to_s }
   output('Total') { object.last.size }
+
+  #
+  # Define a filter to help select which records to find
+  #
+  filter('Just These Objects') do
+    priority 0
+    key :just_these_objects
+    require # Require an input for this filter
+
+    input(:value) {}
+
+    valid? do
+      # Check to see if an input is present
+      validity = !input.blank?
+      !! validity
+    end
+
+    logic do
+      # Perform some logic using the input
+      source.where(["object = #{input}"])
+    end
+  end
 end
